c588c81b0fbea1cb8c7fdcb4e01077290f0c97c4
Kulikoff <kulikoff.ab@yandex.ru>
2021-12-01 22:08:12 +0300

    added files for lesson-5

---
+++ Lesson-5/client.py
@@ -0,0 +1,74 @@
+"""Клиент"""
+import json
+import socket
+import sys
+import time
+
+from collective.general_func import send_message, get_message
+from collective.settings import ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME, DEFAULT_IP_ADDRESS, \
+    DEFAULT_PORT, RESPONSE, ERROR
+import logging
+import log.client_log_config
+
+logger_client = logging.getLogger('app.client')
+
+
+def create_presence(account_name='Guest'):
+    """
+    Функция генерирует запрос о присутсвии клиента
+    """
+    out = {
+        ACTION: PRESENCE,
+        TIME: time.time(),
+        USER: {
+            ACCOUNT_NAME: account_name
+        }
+    }
+    return out
+
+
+def process_ans(message):
+    """
+    Функция разбирает ответ сервера
+    """
+    if RESPONSE in message:
+        if message[RESPONSE] == 200:
+            return '200 : OK'
+        return f'400 : {message[ERROR]}'
+    raise ValueError
+
+
+def main():
+    """
+    Загружает параметры командной строки
+    """
+    try:
+        server_address = sys.argv[1]
+        server_port = int(sys.argv[2])
+        if server_port < 1024 or server_port > 65535:
+            raise ValueError
+    except IndexError:
+        server_address = DEFAULT_IP_ADDRESS
+        server_port = DEFAULT_PORT
+    except ValueError:
+        # print('В качестве порта может быть указано только число в диапазоне 1025 - 65534')
+        logger_client.critical('В качестве порта может быть указано только число в диапазоне 1025 - 65534')
+        sys.exit(1)
+
+    # Инициализация сокета и обмен
+
+    transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    transport.connect((server_address, server_port))
+    message_to_server = create_presence()
+    send_message(transport, message_to_server)
+    try:
+        answer = process_ans(get_message(transport))
+        # print(answer)
+        logger_client.info(answer)
+    except (ValueError, json.JSONDecodeError):
+        # print('Не удалось декодировать сообщение сервера.')
+        logger_client.critical('Не удалось декодировать сообщение сервера.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ Lesson-5/server.py
@@ -0,0 +1,84 @@
+"""Сервер"""
+
+import socket
+import sys
+import json
+from collective.settings import ACTION, ACCOUNT_NAME, RESPONSE, PRESENCE, \
+    TIME, USER, ERROR, DEFAULT_PORT, RESPONSEDEFAULT_IP_ADDRESS, MAX_CONNECTIONS
+from collective.general_func import get_message, send_message
+import logging
+import log.server_log_config
+
+logger_serv = logging.getLogger('app.server')
+
+
+def process_client_message(message):
+    """
+    Обработчик сообщений от клиента
+    """
+    if ACTION in message and message[ACTION] == PRESENCE and TIME in message \
+            and USER in message and message[USER][ACCOUNT_NAME] == 'Guest':
+        return {RESPONSE: 200}
+    return {
+        RESPONSEDEFAULT_IP_ADDRESS: 400,
+        ERROR: 'Bad Request'
+    }
+
+
+def main():
+    """
+    Загрузка параметров командной строки, если нет параметров, то задаем значения по умолчанию.
+    Сначала обрабатываем порт:
+    server.py -p 8888 -a 127.0.0.1
+    """
+    try:
+        if '-p' in sys.argv:
+            listen_port = int(sys.argv[sys.argv.index('-p') + 1])
+        else:
+            listen_port = DEFAULT_PORT
+            if listen_port < 1024 or listen_port > 65535:
+                raise ValueError
+    except IndexError:
+        # print("После параметра '-p' необходимо указать номер порта.")
+        logger_serv.info("После параметра '-p' необходимо указать номер порта.")
+        sys.exit(1)
+
+    # Затем загружаем какой адрес слушать
+    try:
+        if '-a' in sys.argv:
+            listen_address = sys.argv[sys.argv.index('-a') + 1]
+        else:
+            listen_address = ''
+
+    except IndexError:
+        # print("После параметра '-a' необходимо указать адрес, который будет слушать сервер.")
+        logger_serv.info("После параметра '-a' необходимо указать адрес, который будет слушать сервер.")
+        sys.exit(1)
+
+    # Готовим сокет
+
+    transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # transport.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    transport.bind((listen_address, listen_port))
+
+    # Слушаем порт
+
+    transport.listen(MAX_CONNECTIONS)
+
+    while True:
+        client, client_address = transport.accept()
+        try:
+            message_from_client = get_message(client)
+            # print(message_from_client)
+            logger_serv.info(message_from_client)
+            response = process_client_message(message_from_client)
+            send_message(client, response)
+            client.close()
+        except (ValueError, json.JSONDecodeError):
+            # print('Принято не корректное сообщение от клиента.')
+            logger_serv.critical('Принято не корректное сообщение от клиента.')
+            client.close()
+
+
+if __name__ == '__main__':
+    main()
