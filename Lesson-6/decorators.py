f8ce7511b2871c2e3358e306890bf579a796a0a0
Kulikoff <kulikoff.ab@yandex.ru>
2021-12-05 20:04:01 +0300

    added files for lesson-6

---
+++ Lesson-6/decorators.py
@@ -0,0 +1,30 @@
+"""Декораторы"""
+
+import sys
+import logging
+import logs.server_log_config
+import logs.client_log_config
+import traceback
+import inspect
+
+# метод определения модуля, источника запуска.
+# Метод find () возвращает индекс первого вхождения искомой подстроки,
+# если он найден в данной строке.
+# Если его не найдено, - возвращает -1.
+if sys.argv[0].find('client') == -1:
+    # если не клиент то сервер!
+    LOGGER = logging.getLogger('app.server')
+else:
+    LOGGER = logging.getLogger('app.client')
+
+
+def log(func_to_log):
+    """Функция-декоратор"""
+    def log_saver(*args, **kwargs):
+        ret = func_to_log(*args, **kwargs)
+        LOGGER.debug(f'Была вызвана функция {func_to_log.__name__} c параметрами {args}, {kwargs}. '
+                     f'Вызов из модуля {func_to_log.__module__}.'
+                     f'Вызов из функции {traceback.format_stack()[0].strip().split()[-1]}.'
+                     f'Вызов из функции {inspect.stack()[1][3]}')
+        return ret
+    return log_saver
